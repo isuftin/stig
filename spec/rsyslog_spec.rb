a37c43b6b8d7648c21b8bea511e9e1b7a3045487
Ivan Suftin <isuftin@usgs.gov>
2019-10-23 11:19:06 -0500

    Fix rsyslog rspec

--- spec/rsyslog_spec.rb
+++ spec/rsyslog_spec.rb
@@ -3,34 +3,15 @@ require 'spec_helper'
 describe 'stig::rsyslog CentOS 7.x' do
   let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7.3.1611').converge('stig::rsyslog') }
 
-  it 'creates /etc/rsyslog.conf template' do
-    expect(chef_run).to create_template('/etc/rsyslog.conf').with(
-      source: 'etc_rsyslog.conf.erb',
-      owner: 'root',
-      group: 'root',
-      mode: 0o644
-    )
-  end
-
-  it 'excludes restart_syslog execution due to :nothing guard' do
-    exec_restart_syslog = chef_run.execute('restart_syslog')
-    expect(exec_restart_syslog).to do_nothing
+  describe 'includes the third party rsyslog::default recipe' do
+    it { is_expected.to include_recipe('rsyslog::default') }
   end
 end
 
 describe 'stig::rsyslog CentOS 6.x' do
   let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'centos', version: '6.9').converge('stig::rsyslog') }
 
-  it 'creates /etc/rsyslog.conf template' do
-    expect(chef_run).to create_template('/etc/rsyslog.conf').with(
-      source: 'etc_rsyslog.conf.erb',
-      owner: 'root',
-      group: 'root',
-      mode: 0o644
-    )
-  end
-
-  it 'excludes restart_syslog execution due to :nothing guard' do
-    expect(chef_run).to_not run_execute('restart_syslog')
+  describe 'includes the third party rsyslog::default recipe' do
+    it { is_expected.to include_recipe('rsyslog::default') }
   end
 end
